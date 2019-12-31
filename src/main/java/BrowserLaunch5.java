f5b1f14b61cc28f834d95d11b0ad7cc112191b65
zann <naim_khsn@live.co.uk>
2019-12-30 20:32:02 -0500

    added new methods

--- src/main/java/BrowserLaunch5.java
+++ src/main/java/BrowserLaunch5.java
@@ -3,46 +3,52 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class BrowserLaunch4 {
+public class BrowserLaunch5 {
 
     private static WebDriver driver;
 
     public static void main(String[] args) {
-        BrowserLaunch4 browserLaunch4 = new BrowserLaunch4();
-        browserLaunch4.launchBrowser("chrome", "mac");
+        BrowserLaunch5 browserLaunch5 = new BrowserLaunch5();
+        browserLaunch5.launchBrowser("chrome", "mac");
+        browserLaunch5.waitFor(2);
+        //browserLaunch5.searchForJavaBooksAndValidate();
+        //browserLaunch5.loginToAmazon();
+        browserLaunch5.clickOnBestSeller();
+        browserLaunch5.waitFor(5);
+        browserLaunch5.quitBrowser();
+    }
 
-        //Locators :
-        //id, xpath, className, cssSelector, linkText, partialLinkText, name, tagName
+    public void clickOnBestSeller() {
+        //driver.findElement(By.xpath("//a[@href='/gp/bestsellers/?ref_=nav_cs_bestsellers']")).click();
+        driver.findElement(By.linkText("Amazon Rewards Visa Signature Cards"));
 
-        //type
+    }
+
+    public void loginToAmazon() {
+        driver.findElement(By.id("nav-link-accountList")).click();
+        driver.findElement(By.id("ap_email")).sendKeys("testuser@gmail.com");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        driver.findElement(By.id("ap_password")).sendKeys("mypassword");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+    }
+
+    public void searchForJavaBooksAndValidate() {
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java Books");
-
-
-        //click
         driver.findElement(By.xpath("//input[@value='Go']")).click();
-
-        //input[@value='Go']
-        // -- starts with double slash
-        // --tag name of that line
-        // [ --square bracket start
-        //@key='value'
-        // ] --square bracket end
-
-        browserLaunch4.waitFor(2);
         boolean result = driver.findElement(By.xpath("//span[@class='a-color-state a-text-bold']")).isDisplayed();
-
         if (result == true) {
             System.out.println(result + " : element is displayed");
         } else {
             System.out.println(result + " : element is not displayed");
         }
-
-        browserLaunch4.waitFor(5);
-        browserLaunch4.quitBrowser();
+        //span[@id='whatever']
+        //span[text()='Mastering JavaFX 10: Build advanced and visually stunning Java applications']
+        //span[contains(text(),'Mastering JavaFX 10')]
+        driver.findElement(By.xpath("//span[text()='Mastering JavaFX 10: Build advanced and visually stunning Java applications']")).click();
     }
 
+
     public void launchBrowser(String browserName, String os) {
-        //chrome
         if (browserName.equalsIgnoreCase("chrome")) {
             if (os.equalsIgnoreCase("mac")) {
                 System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
@@ -50,9 +56,7 @@ public class BrowserLaunch4 {
                 System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
             }
             driver = new ChromeDriver();
-        }
-        //firefox
-        else if (browserName.equalsIgnoreCase("firefox")) {
+        } else if (browserName.equalsIgnoreCase("firefox")) {
             if (os.equalsIgnoreCase("mac")) {
                 System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
             } else {
@@ -60,7 +64,6 @@ public class BrowserLaunch4 {
             }
             driver = new FirefoxDriver();
         }
-        //browser opens & navigate to the url
         driver.get("https://www.amazon.com");
     }
 
